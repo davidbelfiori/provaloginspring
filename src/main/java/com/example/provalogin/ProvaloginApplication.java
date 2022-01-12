f26a55c1bf318dc0a210bf3277b8b43309cdc309
davidbelfiori <belfiori.david_julian.stud@itisgalileiroma.it>
2022-01-12 18:59:02 +0100

    provaloginspring

---
+++ src/main/java/com/example/provalogin/ProvaloginApplication.java
@@ -0,0 +1,13 @@
+package com.example.provalogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaloginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvaloginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/provalogin/ProvaloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.provalogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaloginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
